9d685fd0eefe101006008f43428015e634a29f81
Pedro Cardoso <pedroalex.p.c@hotmail.com>
2021-12-12 12:29:26 +0000

    readme

---
+++ Api-liga-portuguesa-main/Readme.md
@@ -0,0 +1,19 @@
+API LIGA PORTUGUESA
+
+Instruções de utilização: 
+
+ - Utilizar o comando npm install
+ - Utilizar o comando node server.js
+ - Aceder ao website http://localhost:8080/
+
+Restrições:
+
+ - Um utilizador sem login efetuado consegue apenas ver as imagens do carrossel
+ - Um utilizador com login efetuado tem acesso a todas as abas
+
+Registo de utilizador:
+
+ - Após inserir os dados de registo e pressionar submeter irá ser enviado um link para a consola do Visual Studio Code.
+ - Pressionar ctrl+LMB para aceder ao link.
+ - Copiar o link 
+
--- Api-liga-portuguesa-main/models/nedb.js
+++ Api-liga-portuguesa-main/models/nedb.js
@@ -1,9 +1,8 @@
 const Datastore = require("nedb");
 let db = {};
 db.users = new Datastore("users.db");
-db.disciplinas = new Datastore("disciplinas.db");
 db.users.loadDatabase();
-db.disciplinas.loadDatabase();
+
 
 // Ativa um utilizador (faz um Update)
 exports.crUd_ativar = (confirmationToken) => {
@@ -66,72 +65,3 @@ exports.Crud_registar = (email, password, confirmationToken) => {
     });
   });
 };
-
-exports.Crud = (data) => {
-  // insere um registo
-  db.disciplinas.insert(data);
-  console.log(JSON.stringify(data));
-};
-
-exports.cRud_all = () => {
-  return new Promise((resolve, reject) => {
-    // lê todos os registos
-    db.disciplinas.find({}, (err, dados) => {
-      if (err) {
-        reject("Não há disciplinas para mostrar!");
-      } else {
-        resolve(dados);
-      }
-    });
-  });
-};
-
-exports.cRud_id = (id) => {
-  return new Promise((resolve, reject) => {
-    // busca os registos que contêm a chave
-    db.disciplinas.find(
-      {
-        _id: id,
-      },
-      (err, dados) => {
-        if (err) {
-          reject("Disciplina com o id " + id + " não encontrada!");
-        } else {
-          resolve(dados);
-        }
-      }
-    );
-  });
-};
-
-exports.cRud_key = (criteria) => {
-  return new Promise((resolve, reject) => {
-    // busca os registos que contêm a chave
-    db.disciplinas.find(
-      {
-        $or: [
-          {
-            disciplina: new RegExp(criteria), // RegExp é para usar como expressão regular /criterio/
-          },
-          {
-            docente: new RegExp(criteria),
-          },
-          {
-            curso: new RegExp(criteria),
-          },
-          {
-            ano: Number(criteria),
-          },
-        ],
-      },
-      (err, dados) => {
-        if (err || Object.keys(dados).length == 0) {
-          reject("Não posso mostrar disciplinas!");
-        } else {
-          resolve(dados);
-        }
-      }
-    );
-  });
-};
-
